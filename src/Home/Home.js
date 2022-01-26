6c355e6809ee42ac174700d22774a17a29b057d1
dkimd5 <dkimd5@mail.ru>
2022-01-26 10:43:56 +0300

    content added and styled for home component

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -15,9 +15,26 @@ function Home() {
               <a href='#Home'><li className="nav__items mx__15">Home</li></a>
               <a href='#About'><li className="nav__items mx__15">About</li></a>
               <a href='#Services'><li className="nav__items mx__15">Services</li></a>
+              <a href='#Portfolio'><li className="nav__items mx__15">Portfolio</li></a>
               <a href='#Blog'><li className="nav__items mx__15">Blog</li></a>
               <a href='#Contact'><li className="nav__items mx__15">Contact</li></a>
             </ul>
+          </div>
+        </div>
+        {/* HOME CONTENT */}
+        <div className="container">
+          <div className="home__content">
+            <div className="home__meta">
+              <h1 className="home__text pz__10">
+                WELCOME TO MY PAGE
+              </h1>
+              <h2 className="home__text pz__10">
+                Hi, I'm Dmitrii Kim
+              </h2>
+              <h3 className="home__text sweet">
+                Front-end Developer
+              </h3>
+            </div>
           </div>
         </div>
       </div>
