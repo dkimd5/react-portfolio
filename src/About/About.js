33266ce2f37608eafda974febe0778f4ec01d96d
dkimd5 <dkimd5@mail.ru>
2022-01-26 17:49:27 +0300

    about text added

--- src/About/About.js
+++ src/About/About.js
@@ -1,22 +1,42 @@
-import React from 'react';
-import './About.css';
-import aboutImg from '../img/aboutImg.jpg'
+import React from "react";
+import "./About.css";
+import aboutImg from "../img/aboutImg.jpg";
 
 function About() {
   return (
-        <div className='about component__space'>
-            <div className="container">
-                <div className="row">
-                    <div className="col__2">
-                        <img src={aboutImg} alt="" className='about__img' />
-                    </div>
-                    <div className="col__2">
-                        
-                    </div>
-                </div>
+    <div className="about component__space">
+      <div className="container">
+        <div className="row">
+          <div className="col__2">
+            <img src={aboutImg} alt="" className="about__img" />
+          </div>
+          <div className="col__2">
+            <h1 className="about__heading">About Me</h1>
+            <div className="about__meta">
+              <p className="about__text p__color">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Doloribus iusto magnam fuga corrupti fugiat voluptatum aliquam
+                aut facere cum repellat quis reiciendis sunt deserunt architecto
+                itaque, expedita consectetur at quidem repellendus minima.
+                Minima magnam quia nisi dolores, rem aperiam quas consectetur!
+              </p>
+              <p className="about__text p__color">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Doloribus iusto magnam fuga corrupti fugiat voluptatum aliquam
+                aut facere cum repellat quis reiciendis sunt deserunt architecto
+                itaque, expedita consectetur at quidem repellendus minima.
+                Minima magnam quia nisi dolores, rem aperiam quas consectetur!
+              </p>
+              <p className="about__text p__color">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Doloribus iusto magnam fuga corrupti fugiat voluptatum aliquam
+              </p>
             </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default About;
