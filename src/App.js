4e2ace78d9b23ef97ab697ac90a117787051369b
dkimd5 <dkimd5@mail.ru>
2022-01-27 10:53:10 +0300

    services heading added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import About from "./About/About";
 import "./App.css";
 import Home from "./Home/Home";
+import Service from "./Service/Service";
 
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
+      <Service />
     </div>
-  )
+  );
 }
 
 export default App;
---
+++ src/Service/Service.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Service.css";
+
+function Service() {
+  return (
+    <div className="service component__space">
+      <div className="heading">
+        <h1 className="heading">My Awesome Service</h1>
+        <p className="heading p__color">
+          There are many variations of passages of Lorem Ipsum available
+        </p>
+        <p className="heading p__color">
+          but the majority have suffered alteration.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Service;
