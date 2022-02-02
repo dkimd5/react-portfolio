154a1d73eb9c32e29504aab95853934cfc1d11d1
dkimd5 <dkimd5@mail.ru>
2022-02-02 14:44:32 +0300

    icon's names added

--- src/Skills/Skills.js
+++ src/Skills/Skills.js
@@ -16,45 +16,59 @@ function Skills() {
       <div className="box-container">
         <div className="box__icon">
           <i className="devicon devicon-html5-plain colored"></i>
+          <p>HTML5</p>
         </div>
         <div className="box__icon">
           <i className="devicon devicon-css3-plain colored"></i>
+          <p>CSS3</p>
         </div>
         <div className="box__icon">
           <i className="devicon devicon-javascript-plain colored"></i>
+          <p>JavaScript</p>
         </div>
         <div className="box__icon">
           <i className="devicon devicon-react-original colored"></i>
+          <p>React</p>
         </div>
         <div className="box__icon">
           <i className="devicon devicon-git-plain colored"></i>
+          <p>Git</p>
         </div>
         <div className="box__icon">
           <i className="devicon devicon-github-original devicon-color"></i>
+          <p>Github</p>
         </div>
         <div className="box__icon">
           <i className="devicon devicon-firebase-plain colored"></i>
+          <p>Firebase</p>
         </div>
         <div className="box__icon">
           <i className="devicon devicon-sass-original colored"></i>
+          <p>SASS</p>
         </div>
         <div className="box__icon devicon-color">
           <i className="devicon fas fa-cogs"></i>
+          <p>RESTful APIs</p>
         </div>
         <div className="box__icon">
           <i className="devicon devicon-vscode-plain colored"></i>
+          <p>VS Code</p>
         </div>
         <div className="box__icon devicon-color2">
           <i className="devicon fas fa-terminal"></i>
+          <p>Command Line</p>
         </div>
         <div className="box__icon">
           <i className="devicon devicon-webpack-plain colored"></i>
+          <p>Webpack</p>
         </div>
         <div className="box__icon">
           <i className="devicon devicon-redux-original colored"></i>
+          <p>Redux</p>
         </div>
         <div className="box__icon">
           <i className="devicon devicon-materialui-plain colored"></i>
+          <p>Material-UI</p>
         </div>
         <div className="box__icon">
           <div className="container-img-i i-responsive">
@@ -63,13 +77,13 @@ function Skills() {
               alt="icon of tablet, phone and browser window."
             />
           </div>
-          {/* <p>Responsive Websites</p> */}
+          <p>Responsive Websites</p>
         </div>
         <div className="box__icon">
           <div className="container-img-i i-w3c">
             <img src={w3cIcon} alt="W3C logo" />
           </div>
-          {/* <p>Web Accessibility</p> */}
+          <p>Web Accessibility</p>
         </div>
       </div>
     </div>
