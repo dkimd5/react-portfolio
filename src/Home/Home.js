7a80b6f61e2c567394408c4ebcf3099bf45003fb
dkimd5 <dkimd5@mail.ru>
2022-02-02 09:40:23 +0300

    toggle navbar added

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import logo from "../img/logo.png";
 
@@ -8,6 +8,9 @@ function Home() {
     const header = document.querySelector(".header");
     header.classList.toggle("active", window.scrollY > 0);
   });
+
+  //Toggle menu
+  const [show, setShow] = useState(true);
 
   return (
     <div className="home" id="Home">
@@ -38,6 +41,47 @@ function Home() {
               </a>
             </ul>
           </div>
+          {/* Toggle menu */}
+          <div className="toggle__menu">
+            <svg
+              onClick={() => setShow(!show)}
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-justify white pointer"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"
+              />
+            </svg>
+          </div>
+          {show ? (
+            <div className="sideNavbar">
+              <ul className="sidebar d__flex">
+                <li className="sideNavbar">
+                  <a href="#home">Home</a>
+                </li>
+                <li className="sideNavbar">
+                  <a href="#about">About</a>
+                </li>
+                <li className="sideNavbar">
+                  <a href="#services">Services</a>
+                </li>
+                <li className="sideNavbar">
+                  <a href="#projects">Projects</a>
+                </li>
+                <li className="sideNavbar">
+                  <a href="#blog">Blog</a>
+                </li>
+                <li className="sideNavbar">
+                  <a href="#contact">Contact</a>
+                </li>
+              </ul>
+            </div>
+          ) : null}
         </div>
         {/* HOME CONTENT */}
         <div className="container">
