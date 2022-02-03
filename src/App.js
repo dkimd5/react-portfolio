965eed7b5f31be565c061d2a680a0d43d2533124
dkimd5 <dkimd5@mail.ru>
2022-02-03 11:58:01 +0300

    navbar content and logic moved from comp to navbar component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
 import Projects from "./components/Projects/Projects";
 import Service from "./components/Service/Service";
 import Skills from "./components/Skills/Skills";
@@ -10,6 +11,7 @@ import Skills from "./components/Skills/Skills";
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Home />
       <About />
       <Service />
