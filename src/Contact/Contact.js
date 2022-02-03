d6f1ea7b21d50c4e37a4509b5765e768bfccb060
dkimd5 <dkimd5@mail.ru>
2022-02-03 11:30:48 +0300

    section container div added to components

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -4,43 +4,45 @@ import "./Contact.css";
 function Contact() {
   return (
     <div className="contact component__space" id="Contact">
-      <div className="row">
-        <div className="col__2">
-          <div className="contact__box">
-            <div className="contact__meta">
-              <h1 className="hire__text">Hire Me.</h1>
-              <p className="hire__text white">
-                I am available for work. Connect with me via phone:
-              </p>
-              <p className="hire__text white">
-                <strong>+7777 77 777</strong> or email{" "}
-                <strong>admin@example.com</strong>
-              </p>
-            </div>
-            <div className="input__box">
-              <input
-                type="text"
-                className="contact name"
-                placeholder="Your name *"
-              />
-              <input
-                type="text"
-                className="contact email"
-                placeholder="Your Email *"
-              />
-              <input
-                type="text"
-                className="contact subject"
-                placeholder="Write a Subject"
-              />
-              <textarea
-                name="message"
-                id="message"
-                placeholder="Write Your message"
-              ></textarea>
-              <button className="btn contact pointer" type="submit">
-                Submit
-              </button>
+      <div className="section__container">
+        <div className="row">
+          <div className="col__2">
+            <div className="contact__box">
+              <div className="contact__meta">
+                <h1 className="hire__text">Hire Me.</h1>
+                <p className="hire__text white">
+                  I am available for work. Connect with me via phone:
+                </p>
+                <p className="hire__text white">
+                  <strong>+7777 77 777</strong> or email{" "}
+                  <strong>admin@example.com</strong>
+                </p>
+              </div>
+              <div className="input__box">
+                <input
+                  type="text"
+                  className="contact name"
+                  placeholder="Your name *"
+                />
+                <input
+                  type="text"
+                  className="contact email"
+                  placeholder="Your Email *"
+                />
+                <input
+                  type="text"
+                  className="contact subject"
+                  placeholder="Write a Subject"
+                />
+                <textarea
+                  name="message"
+                  id="message"
+                  placeholder="Write Your message"
+                ></textarea>
+                <button className="btn contact pointer" type="submit">
+                  Submit
+                </button>
+              </div>
             </div>
           </div>
         </div>
