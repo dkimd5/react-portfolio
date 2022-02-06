f21f196f054312fab35cb6dcac78756ad22c48b6
dkimd5 <dkimd5@mail.ru>
2022-02-06 11:29:15 +0300

    certificates images and links added

--- src/components/Certificates/Certificates.js
+++ src/components/Certificates/Certificates.js
@@ -1,32 +1,37 @@
 import React from "react";
 import "./Certificates.css";
-import certificateReact from "../../images/CertificateReact.png";
+import certificateGit from "../../images/CertificateGit.jpeg";
+import certificateHTMLandCSS from "../../images/CertificateHTMLandCSS.jpeg";
+import certificateJS1 from "../../images/CertificateJS1.jpeg";
+import certificateJS2 from "../../images/CertificateJS2.jpeg";
+import certificateDB from "../../images/CertificateDatabase.jpeg";
+import certificateReact from "../../images/CertificateReact.jpeg";
 
 const certificatesList = [
   {
-    name: "React",
-    link: "https://gb.ru/certificates/1261795.en",
-    image: certificateReact,
+    name: "Git",
+    link: "https://gb.ru/certificates/961166.en",
+    image: certificateGit,
   },
   {
-    name: "React",
-    link: "https://gb.ru/certificates/1261795.en",
-    image: certificateReact,
+    name: "HTML5 & CSS3",
+    link: "https://gb.ru/certificates/1060563.en",
+    image: certificateHTMLandCSS,
   },
   {
-    name: "React",
-    link: "https://gb.ru/certificates/1261795.en",
-    image: certificateReact,
+    name: "JavaScript1",
+    link: "https://gb.ru/certificates/1151323.en",
+    image: certificateJS1,
   },
   {
-    name: "React",
-    link: "https://gb.ru/certificates/1261795.en",
-    image: certificateReact,
+    name: "JavaScript2",
+    link: "https://gb.ru/certificates/1211414.en",
+    image: certificateJS2,
   },
   {
-    name: "React",
-    link: "https://gb.ru/certificates/1261795.en",
-    image: certificateReact,
+    name: "Database Management",
+    link: "https://gb.ru/certificates/1222062.en#",
+    image: certificateDB,
   },
   {
     name: "React",
