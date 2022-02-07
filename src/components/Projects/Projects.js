cd271b462be63c4d9f01d0483b94aa193672e74f
dkimd5 <dkimd5@mail.ru>
2022-02-07 15:38:36 +0300

    projects html spread replaced with map method

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -56,120 +56,25 @@ function Projects() {
           </p>
         </div>
         <div className="row">
-          <div className="col__3">
-            <div className="project__box">
-              <div className="project__box__img">
-                <div className="project__img__box">
-                  <img src={Project1} alt="" className="project__img" />
-                </div>
-                <div className="mask__effect"></div>
-                <div className="project__meta">
-                  <h5 className="project__text">Development</h5>
-                  <h4 className="project__text">
-                    Getting ticket to the big show
-                  </h4>
-                  <a href="#" className="project__btn">
-                    View Details
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col__3">
-            <div className="project__box">
-              <div className="project__box__img">
-                <div className="project__img__box">
-                  <img src={Project2} alt="" className="project__img" />
-                </div>
-                <div className="mask__effect"></div>
-                <div className="project__meta">
-                  <h5 className="project__text">Development</h5>
-                  <h4 className="project__text">
-                    Getting ticket to the big show
-                  </h4>
-                  <a href="#" className="project__btn">
-                    View Details
-                  </a>
+          {projectsList.map(({ name, deployLink, githubLink, image }) => (
+            <div className="col__3">
+              <div className="project__box">
+                <div className="project__box__img">
+                  <div className="project__img__box">
+                    <img src={image} alt="" className="project__img" />
+                  </div>
+                  <div className="mask__effect"></div>
+                  <div className="project__meta">
+                    <h5 className="project__text">Development</h5>
+                    <h4 className="project__text">{name}</h4>
+                    <a href="#" className="project__btn">
+                      View Details
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="col__3">
-            <div className="project__box">
-              <div className="project__box__img">
-                <div className="project__img__box">
-                  <img src={Project3} alt="" className="project__img" />
-                </div>
-                <div className="mask__effect"></div>
-                <div className="project__meta">
-                  <h5 className="project__text">Development</h5>
-                  <h4 className="project__text">
-                    Getting ticket to the big show
-                  </h4>
-                  <a href="#" className="project__btn">
-                    View Details
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col__3">
-            <div className="project__box">
-              <div className="project__box__img">
-                <div className="project__img__box">
-                  <img src={Project4} alt="" className="project__img" />
-                </div>
-                <div className="mask__effect"></div>
-                <div className="project__meta">
-                  <h5 className="project__text">Development</h5>
-                  <h4 className="project__text">
-                    Getting ticket to the big show
-                  </h4>
-                  <a href="#" className="project__btn">
-                    View Details
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col__3">
-            <div className="project__box">
-              <div className="project__box__img">
-                <div className="project__img__box">
-                  <img src={Project3} alt="" className="project__img" />
-                </div>
-                <div className="mask__effect"></div>
-                <div className="project__meta">
-                  <h5 className="project__text">Development</h5>
-                  <h4 className="project__text">
-                    Getting ticket to the big show
-                  </h4>
-                  <a href="#" className="project__btn">
-                    View Details
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col__3">
-            <div className="project__box">
-              <div className="project__box__img">
-                <div className="project__img__box">
-                  <img src={Project4} alt="" className="project__img" />
-                </div>
-                <div className="mask__effect"></div>
-                <div className="project__meta">
-                  <h5 className="project__text">Development</h5>
-                  <h4 className="project__text">
-                    Getting ticket to the big show
-                  </h4>
-                  <a href="#" className="project__btn">
-                    View Details
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
           {/* <div className="view__more__btn">
             <button className="view__more btn">View more</button>
           </div> */}
