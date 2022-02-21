4a50ecfaec4841d64103fc55706950262d7e0509
dkimd5 <dkimd5@mail.ru>
2022-02-21 10:38:07 +0300

    Style navbar and side-navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -7,33 +7,35 @@ function Navbar() {
   return (
     <div className="header">
       <div className="section__container">
-        <div className="logo">
-          <a href="#">
-            <img src={logo} alt="logo" />
-          </a>
-        </div>
-        <ul className="nav-menu">
-          <li className="nav__items">
-            <a href="#Home">Home</a>
-          </li>
-          <li className="nav__items">
-            <a href="#About">About</a>
-          </li>
-          <li className="nav__items">
-            <a href="#Projects">Projects</a>
-          </li>
-          <li className="nav__items">
-            <a href="#Certificates">Certificates</a>
-          </li>
-          <li className="nav__items">
-            <a href="#Skills">Skills</a>
-          </li>
-          <li className="nav__items">
-            <a href="#Contact">Contact</a>
-          </li>
-        </ul>
-        <div className="hamburger">
-          <FaBars />
+        <div className="header-wrp">
+          <div className="logo">
+            <a href="#">
+              <img src={logo} alt="logo" />
+            </a>
+          </div>
+          <ul className="nav-menu">
+            <li className="nav__items">
+              <a href="#Home">Home</a>
+            </li>
+            <li className="nav__items">
+              <a href="#About">About</a>
+            </li>
+            <li className="nav__items">
+              <a href="#Projects">Projects</a>
+            </li>
+            <li className="nav__items">
+              <a href="#Certificates">Certificates</a>
+            </li>
+            <li className="nav__items">
+              <a href="#Skills">Skills</a>
+            </li>
+            <li className="nav__items">
+              <a href="#Contact">Contact</a>
+            </li>
+          </ul>
+          <div className="hamburger">
+            <FaBars size={20} className="hamburger-lines" />
+          </div>
         </div>
       </div>
     </div>
