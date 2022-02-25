cca1bc0b1f09e260c7527ac8a8343e1977773ada
dkimd5 <dkimd5@mail.ru>
2022-02-25 10:03:33 +0300

    Delete old styles and wrappers, add new

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,46 +5,42 @@ function Contact() {
   return (
     <div className="contact component__space" id="Contact">
       <div className="section__container">
-        <div className="row">
-          <div className="col__2">
-            <div className="contact__box">
-              <div className="contact__meta">
-                <h1 className="contact__heading">Hire Me</h1>
-                <p className="hire__text white">
-                  I am available for work. Connect with me via phone:
-                </p>
-                <p className="hire__text white">
-                  <strong>+7777 77 777</strong> or email{" "}
-                  <strong>admin@example.com</strong>
-                </p>
-              </div>
-              <div className="input__box">
-                <input
-                  type="text"
-                  className="contact name"
-                  placeholder="Your name *"
-                />
-                <input
-                  type="text"
-                  className="contact email"
-                  placeholder="Your Email *"
-                />
-                <input
-                  type="text"
-                  className="contact subject"
-                  placeholder="Write a Subject"
-                />
-                <textarea
-                  name="message"
-                  id="message"
-                  placeholder="Write Your message"
-                ></textarea>
-                <button className="btn contact pointer" type="submit">
-                  Submit
-                </button>
-              </div>
-            </div>
-          </div>
+        <h1 className="heading">Get In Touch</h1>
+        <div className="contact__content__wrp">
+          <div className="contact__form"></div>
+          <p className="p__text">
+            Say hi, ask for my resume or see if we can build something amazing
+            together? I'd love to hear from you!
+            <br />
+            <br />
+            Fill in your info in the form below and I look forward to hearing
+            from you!
+          </p>
+          <form>
+            <input
+              type="text"
+              className="contact name"
+              placeholder="Your name *"
+            />
+            <input
+              type="text"
+              className="contact email"
+              placeholder="Your Email *"
+            />
+            <input
+              type="text"
+              className="contact subject"
+              placeholder="Write a Subject"
+            />
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Write Your message"
+            ></textarea>
+            <button className="btn contact" type="submit">
+              Submit
+            </button>
+          </form>
         </div>
       </div>
     </div>
