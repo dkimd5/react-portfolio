698205cba83831e586eb8833899121c1eb331727
dkimd5 <dkimd5@mail.ru>
2022-02-25 17:38:45 +0300

    Style footer, set margin bottom 0 for p, cause bt

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,8 @@ import "./Footer.css";
 function Footer() {
   return (
     <div className="footer">
-      <div className="section__container d__flex align__items__center justify__content__space__between pz-10">
-        <span className="copyright">Kim Dmitrii</span>
+      <div className="section__container">
+        <p className="copyright">© 2022 | Coded by Dmitrii Kim</p>
       </div>
     </div>
   );
