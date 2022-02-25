66ee652ee3fc696c8f061001cca0c0ae5aa5a3ac
dkimd5 <dkimd5@mail.ru>
2022-02-25 10:19:39 +0300

    Add width for form and margin bottom for textarea

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -7,40 +7,41 @@ function Contact() {
       <div className="section__container">
         <h1 className="heading">Get In Touch</h1>
         <div className="contact__content__wrp">
-          <div className="contact__form"></div>
-          <p className="p__text">
-            Say hi, ask for my resume or see if we can build something amazing
-            together? I'd love to hear from you!
-            <br />
-            <br />
-            Fill in your info in the form below and I look forward to hearing
-            from you!
-          </p>
-          <form>
-            <input
-              type="text"
-              className="contact name"
-              placeholder="Your name*"
-            />
-            <input
-              type="text"
-              className="contact email"
-              placeholder="Your Email*"
-            />
-            <input
-              type="text"
-              className="contact subject"
-              placeholder="Write a Subject"
-            />
-            <textarea
-              name="message"
-              id="message"
-              placeholder="Write Your message"
-            ></textarea>
-            <button className="btn contact" type="submit">
-              Submit
-            </button>
-          </form>
+          <div className="contact__form">
+            <p className="p__text">
+              Say hi, ask for my resume or see if we can build something amazing
+              together? I'd love to hear from you!
+              <br />
+              <br />
+              Fill in your info in the form below and I look forward to hearing
+              from you!
+            </p>
+            <form>
+              <input
+                type="text"
+                className="contact name"
+                placeholder="Your name*"
+              />
+              <input
+                type="text"
+                className="contact email"
+                placeholder="Your Email*"
+              />
+              <input
+                type="text"
+                className="contact subject"
+                placeholder="Write a Subject"
+              />
+              <textarea
+                name="message"
+                id="message"
+                placeholder="Write Your message"
+              ></textarea>
+              <button className="btn contact" type="submit">
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
