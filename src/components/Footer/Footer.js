cc66aa48ae6657a2d30394d57b741e7cfe15bc6f
dkimd5 <dkimd5@mail.ru>
2022-02-25 17:32:55 +0300

    Delete old styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,10 @@
 import React from "react";
-// import footerImg from "../../images/logo(1).png";
 import "./Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
       <div className="section__container d__flex align__items__center justify__content__space__between pz-10">
-        {/* <img src={footerImg} alt="" className="footer__img pointer" /> */}
         <span className="copyright">Kim Dmitrii</span>
       </div>
     </div>
