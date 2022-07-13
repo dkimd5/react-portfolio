c162de1bcb6b083081f4a8a2169f74cd86d9731c
dkimd5 <dkimd5@mail.ru>
2022-07-13 10:57:05 +0300

    Change h3 title

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -2,22 +2,22 @@ import React from "react";
 import "./Skills.css";
 
 const iconsList = [
-  { className: "devicon devicon-html5-plain colored", title: "HTML5" },
-  { className: "devicon devicon-css3-plain colored", title: "CSS3" },
-  { className: "devicon devicon-javascript-plain colored", title: "JavaScript" },
-  { className: "devicon devicon-typescript-plain colored", title: "TypeScript" },
-  { className: "devicon devicon-react-original colored", title: "React" },
+  { className: "devicon devicon-html5-plain colored", title: "HTML5", color: "" },
+  { className: "devicon devicon-css3-plain colored", title: "CSS3", color: "" },
+  { className: "devicon devicon-javascript-plain colored", title: "JavaScript", color: "" },
+  { className: "devicon devicon-typescript-plain colored", title: "TypeScript", color: "" },
+  { className: "devicon devicon-react-original colored", title: "React", color: "" },
   { className: "devicon devicon-nextjs-original", title: "Nextjs", color: "devicon-color" },
-  { className: "devicon devicon-redux-original colored", title: "Redux" },
-  { className: "devicon devicon-webpack-plain colored", title: "Webpack" },
-  { className: "devicon devicon-git-plain colored", title: "Git" },
-  { className: "devicon devicon-github-original devicon-color", title: "Github" },
-  { className: "devicon devicon-firebase-plain colored", title: "Firebase" },
-  { className: "devicon devicon-sass-original colored", title: "SASS" },
+  { className: "devicon devicon-redux-original colored", title: "Redux", color: "" },
+  { className: "devicon devicon-webpack-plain colored", title: "Webpack", color: "" },
+  { className: "devicon devicon-git-plain colored", title: "Git", color: "" },
+  { className: "devicon devicon-github-original devicon-color", title: "Github", color: "" },
+  { className: "devicon devicon-firebase-plain colored", title: "Firebase", color: "" },
+  { className: "devicon devicon-sass-original colored", title: "SASS", color: "" },
   { className: "devicon fas fa-cogs", title: "RESTful APIs", color: "devicon-color" },
-  { className: "devicon devicon-vscode-plain colored", title: "VS Code" },
+  { className: "devicon devicon-vscode-plain colored", title: "VS Code", color: "" },
   { className: "devicon fas fa-terminal", title: "Command Line", color: "devicon-color2" },
-  { className: "devicon devicon-materialui-plain colored", title: "Material-UI" },
+  { className: "devicon devicon-materialui-plain colored", title: "Material-UI", color: "" },
 ];
 
 function Skills() {
@@ -38,7 +38,7 @@ function Skills() {
         </ul>
         <div className="skills__h3__wrap">
           <h3>Currently working on:</h3>
-          <p>Improving my skills in, and understanding of vanilla JavaScript and React.</p>
+          <p>Improving my skills in, and understanding of vanilla JavaScript, React and Nextjs.</p>
         </div>
       </div>
     </div>
