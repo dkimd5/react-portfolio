54fbbe377ef6eb77d1ab9e71b7e32e611abfa14d
dkimd5 <dkimd5@mail.ru>
2022-07-13 10:19:30 +0300

    Create icons list in array and map it inside skills component

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,6 +1,25 @@
 import React from "react";
 import "./Skills.css";
 
+const iconsList = [
+  { className: "devicon devicon-html5-plain colored", title: "HTML5" },
+  { className: "devicon devicon-css3-plain colored", title: "CSS3" },
+  { className: "devicon devicon-javascript-plain colored", title: "JavaScript" },
+  { className: "devicon devicon-react-original colored", title: "React" },
+  { className: "devicon devicon-git-plain colored", title: "Git" },
+  { className: "devicon devicon-github-original devicon-color", title: "Github" },
+  { className: "devicon devicon-firebase-plain colored", title: "Firebase" },
+  { className: "devicon devicon-sass-original colored", title: "SASS" },
+  { className: "devicon fas fa-cogs", title: "RESTful APIs", color: "devicon-color" },
+  { className: "devicon devicon-vscode-plain colored", title: "VS Code" },
+  { className: "devicon fas fa-terminal", title: "Command Line", color: "devicon-color2" },
+  { className: "devicon devicon-webpack-plain colored", title: "Webpack" },
+  { className: "devicon devicon-redux-original colored", title: "Redux" },
+  { className: "devicon devicon-materialui-plain colored", title: "Material-UI" },
+  { className: "devicon-nextjs-original", title: "Nextjs", color: "devicon-color" },
+  { className: "devicon-typescript-plain colored", title: "TypeScript" },
+];
+
 function Skills() {
   return (
     <div className="skills component__space" id="Skills">
@@ -10,62 +29,12 @@ function Skills() {
           The skills, tools and technologies I use to bring your products to life:
         </h2>
         <ul className="box-container">
-          <li className="box__icon">
-            <i className="devicon devicon-html5-plain colored"></i>
-            <p>HTML5</p>
-          </li>
-          <li className="box__icon">
-            <i className="devicon devicon-css3-plain colored"></i>
-            <p>CSS3</p>
-          </li>
-          <li className="box__icon">
-            <i className="devicon devicon-javascript-plain colored"></i>
-            <p>JavaScript</p>
-          </li>
-          <li className="box__icon">
-            <i className="devicon devicon-react-original colored"></i>
-            <p>React</p>
-          </li>
-          <li className="box__icon">
-            <i className="devicon devicon-git-plain colored"></i>
-            <p>Git</p>
-          </li>
-          <li className="box__icon">
-            <i className="devicon devicon-github-original devicon-color"></i>
-            <p>Github</p>
-          </li>
-          <li className="box__icon">
-            <i className="devicon devicon-firebase-plain colored"></i>
-            <p>Firebase</p>
-          </li>
-          <li className="box__icon">
-            <i className="devicon devicon-sass-original colored"></i>
-            <p>SASS</p>
-          </li>
-          <li className="box__icon devicon-color">
-            <i className="devicon fas fa-cogs"></i>
-            <p>RESTful APIs</p>
-          </li>
-          <li className="box__icon">
-            <i className="devicon devicon-vscode-plain colored"></i>
-            <p>VS Code</p>
-          </li>
-          <li className="box__icon devicon-color2">
-            <i className="devicon fas fa-terminal"></i>
-            <p>Command Line</p>
-          </li>
-          <li className="box__icon">
-            <i className="devicon devicon-webpack-plain colored"></i>
-            <p>Webpack</p>
-          </li>
-          <li className="box__icon">
-            <i className="devicon devicon-redux-original colored"></i>
-            <p>Redux</p>
-          </li>
-          <li className="box__icon">
-            <i className="devicon devicon-materialui-plain colored"></i>
-            <p>Material-UI</p>
-          </li>
+          {iconsList.map(({ className, title, color }) => (
+            <li className={`box__icon ${color} `}>
+              <i className={className}></i>
+              <p>{title}</p>
+            </li>
+          ))}
         </ul>
         <div className="skills__h3__wrap">
           <h3>Currently working on:</h3>
